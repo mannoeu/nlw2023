bb19fa873cb4f373593608b32d19e578a5a1266e
mannoeu <emmanuel_costasilva@hotmail.com>
2023-01-17 11:05:57 -0300

    starting setup server, web and mobile project

---
+++ server/src/server.ts
@@ -0,0 +1,20 @@
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+
+const app = fastify();
+const prisma = new PrismaClient();
+
+app.register(cors);
+
+app.get("/", async () => {
+  const habits = await prisma.habit.findMany();
+
+  return habits;
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log("Server running on port 3333"));
---
+++ web/src/App.tsx
@@ -0,0 +1,7 @@
+import "./styles/global.css";
+
+function App() {
+  return <div className="App">Hello World</div>;
+}
+
+export default App;
